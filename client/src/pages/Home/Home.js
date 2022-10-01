21b27c8e1e74d444b03d5c17782777543cf519a4
msx-dev <maxim.sramek@gmail.com>
2022-10-01 17:44:21 +0200

    Started working on the homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,27 @@
 import {Link} from "react-router-dom";
 import "./Home.css";
+import Logo from "../../public/Logo/Logo.png";
 
 
 function Home() {
   return (
     <div className="App">
-      <h1>Testing homepage</h1>
+      <div className="navigation-bar">
+        <div id="navigation-container">
+          <div className="logo-container">
+            <img className="navigation-logo" src={Logo}/>
+            <h1 className="logo-name">SurfLog</h1>
+          </div>
+          <ul>
+            <li><a href="/app">Features</a></li>
+            <li><a href="/app">About</a></li>
+            <li><a href="/app">App</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="main-hero">
+
+      </div>
       <Link to="/app">Surfel App</Link>
     </div>
   );
